4e876c6a73602505c072dcaa157786ffdb455753
Arpit <arpitsrivastav2023@gmail.com>
2026-05-16 08:37:29 +0530

    AI summary fix-AI

--- dist/services/ai/gemini.service.js
+++ dist/services/ai/gemini.service.js
@@ -4,7 +4,7 @@ exports.generatePRSummary = void 0;
 const generative_ai_1 = require("@google/generative-ai");
 const genAI = new generative_ai_1.GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash-latest",
+    model: "gemini-pro",
 });
 const generatePRSummary = async (prData) => {
     const prompt = `
