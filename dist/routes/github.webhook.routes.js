1f481646d857ff7eaecdcb48077d56898a825adf
Arpit <arpitsrivastav2023@gmail.com>
2026-05-16 11:15:37 +0530

    add webhook get route

--- dist/routes/github.webhook.routes.js
+++ dist/routes/github.webhook.routes.js
@@ -2,6 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
+router.get("/", (req, res) => {
+    return res.json({
+        message: "Webhook route working",
+    });
+});
 router.post("/", async (req, res) => {
     console.log("GitHub webhook received");
     return res.status(200).json({
--- src/routes/github.webhook.routes.ts
+++ src/routes/github.webhook.routes.ts
@@ -2,6 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  return res.json({
+    message: "Webhook route working",
+  });
+});
+
 router.post("/", async (req, res) => {
   console.log("GitHub webhook received");
 
